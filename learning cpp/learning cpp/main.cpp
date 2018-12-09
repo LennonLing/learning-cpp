1c0fb115fbc83e78eec4fffc4f9e0e941dbf1e22
lennon <bin.ling.2018@gmail.com>
2018-12-09 18:29:38 +0800

    first commit

---
+++ learning cpp/learning cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learning cpp
+//
+//  Created by ling on 2018/12/9.
+//  Copyright © 2018 com.lennon.www. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
